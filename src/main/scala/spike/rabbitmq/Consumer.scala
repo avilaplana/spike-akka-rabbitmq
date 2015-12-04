26466e7c9560270466204e87dca9d22351db9ade
tomas.klubal <tomas.klubal@bskyb.com>
2015-12-04 15:51:49 +0000

    adding a consumer pool with  5 consumers

--- src/main/scala/spike/rabbitmq/Consumer.scala
+++ src/main/scala/spike/rabbitmq/Consumer.scala
@@ -4,20 +4,27 @@ import akka.actor.ActorRef
 import com.rabbitmq.client.Channel
 import com.thenewmotion.akka.rabbitmq._
 
-object Consumer extends App with SystemActor {
+class Consumer(id: String) extends SystemActor {
 
-  def setupSubscriber(channel: Channel, self: ActorRef) {
+  println(s"Consumer $id starting.....")
+
+  connection ! CreateChannel(ChannelActor.props(setupSubscriber), Some(s"subscriber-$id"))
+
+  private def setupSubscriber(channel: Channel, self: ActorRef) {
     channel.queueDeclare(queueName, durable, exclusive, autoDelete, null);
     channel.queueBind(queueName, exchange, routingKey)
     val consumer = new DefaultConsumer(channel) {
       override def handleDelivery(consumerTag: String, envelope: Envelope, properties: BasicProperties, body: Array[Byte]) {
-        println(s"consumer receiving ${fromBytes(body)}")
+        println(s"consumer $id receiving ${fromBytes(body)}")
       }
     }
     channel.basicConsume(queueName, true, consumer)
   }
+  private def fromBytes(x: Array[Byte]) = new String(x, "UTF-8").toLong
+}
 
-  connection ! CreateChannel(ChannelActor.props(setupSubscriber), Some("subscriber"))
 
-  def fromBytes(x: Array[Byte]) = new String(x, "UTF-8").toLong
+
+object ConsumerPool extends App {
+  Range(1,5).map(id => new Consumer(s"CONSUMER-$id"))
 }
